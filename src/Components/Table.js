b1609c5fb5bac73fefd3fac7d14739ffbad6027d
David Fertitta <Davidj.fertitta@gmail.com>
2023-05-11 13:36:49 -0500

    organizing data for left column

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { v2data } from '../Assets/Data/V2';
-import printIcon from '../Assets/Icons/printIcon.svg';
-import ebookIcon from '../Assets/Icons/ebookIcon.svg';
-import pptIcon from '../Assets/Icons/pptIcon.svg';
 import '../Styles/Table.css';
 
 export const Table = () => {
+    const data = v2data;
+    // ***** change this to switch between data sets
+
+    const [titles, setTitles] = useState();
+    const [genres, setGenres] = useState();
+    const [authors, setAuthors] = useState();
+    const [lexiles, setLexiles] = useState();
+    const [synops, setSynops] = useState();
+
+    useEffect(() => {
+        let justTitles = data.map((item) => item.Title);
+        setTitles(justTitles);
+
+        let justGenres = data.map((item) => item.Genre);
+        setGenres(justGenres);
+
+        let justAuthors = data.map((item) => item.Author);
+        setAuthors(justAuthors);
+
+        let justLexiles = data.map((item) => item.Lexile);
+        setLexiles(justLexiles);
+
+        let justSynops = data.map((item) => item.Synopsis);
+        setSynops(justSynops);
+    }, [data]);
+
     return (
         <div className='table-container all-columns col-14'>
             <div className='table-headers all-columns col-7'>
@@ -24,6 +47,7 @@ export const Table = () => {
                     <h5 className='header resources'>Resources</h5>
                 </div>
             </div>
+            <div className='table-body lex-col'></div>
         </div>
     );
 };
